700d1b03f1dba485e726abb2c66a9205e32063d2
Christiaanvdm <christiaan@kartoza.com>
2019-08-31 08:32:59 +0200

    Add shipinput file

---
+++ Assets/FreelancerFlightExample/Scripts/Ship/ShipInput.cs
@@ -0,0 +1,254 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+
+
+    /// <summary>
+    /// Class specifically to deal with input.
+    /// </summary>
+    public class ShipInput : NetworkBehaviour
+    {
+        [Tooltip("How far the ship will bank when turning.")]
+        [SerializeField] private float bankLimit = 35f;
+
+        [Tooltip("Sensitivity in the pitch axis.\n\nIt's best to play with this value until you can get something the results in full input when at the edge of the screen.")]
+        [SerializeField] private float pitchSensitivity = 2.5f;
+        [Tooltip("Sensitivity in the yaw axis.\n\nIt's best to play with this value until you can get something the results in full input when at the edge of the screen.")]
+        [SerializeField] private float yawSensitivity = 2.5f;
+        [Tooltip("Sensitivity in the roll axis.\n\nTweak to make responsive enough.")]
+        [SerializeField] private float rollSensitivity = 1f;
+
+        [Range(-1, 1)]
+        [SerializeField] private float pitch;
+        [Range(-1, 1)]
+        [SerializeField] private float yaw;
+        [Range(-1, 1)]
+        [SerializeField] private float roll;
+        [Range(-1, 1)]
+        [SerializeField] private float strafe;
+        [Range(0, 1)]
+        [SerializeField] private float throttle;
+
+        // How quickly the throttle reacts to input.
+        private const float THROTTLE_SPEED = 0.5f;
+
+        public bool DisableSteering;
+
+        public float Pitch { get { return pitch; } }
+        public float Yaw { get { return yaw; } }
+        public float Roll { get { return roll; } }
+        public float Strafe { get { return strafe; } }
+        public float Throttle { get { return throttle; } }
+
+        private Camera mainCam;
+
+        private Rigidbody rb;
+
+        void OnDisable()
+        {
+            if (isLocalPlayer)
+            {
+                // Disable the local player's camera
+                //GetComponentInChildren<Camera>().enabled = false;
+                mainCam.enabled = false;
+                // Re-enable the main camera when Stop is pressed in the HUD
+                if (mainCam != null) mainCam.enabled = true;
+            }
+        }
+
+        private void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                return;
+            }
+          
+
+            //if ((Input.mousePosition.x > Screen.width || Input.mousePosition.x < 0) || (Input.mousePosition.y > Screen.height || Input.mousePosition.y < 0))
+            //{
+            //    return;
+            //}
+
+            strafe = Input.GetAxis("Horizontal");
+            if (!DisableSteering && base.isLocalPlayer)
+            {
+                SetStickCommandsUsingAutopilot();
+                UpdateMouseWheelThrottle();
+                UpdateKeyboardThrottle(KeyCode.W, KeyCode.S);
+                if (Input.GetAxis("Fire1") > 0)
+                {
+                    if (allowfire)
+                    {
+                        StartCoroutine(Fire());
+                    }
+                }
+            }
+            
+		}
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+            mainCam = GetComponentInChildren<Camera>();
+
+            rb = GetComponent<Rigidbody>();
+            // Grab a refernce to the main camera so we can enable it again in OnDisable
+            //mainCam = GetComponentInChildren<CameraRig>().cam;
+        
+
+            // Turn off the main camera because the Player prefab has its own camera
+            //mainCam.enabled = false;
+
+            //// Enable the local player's camera
+            //GetComponentInChildren<Camera>().enabled = true;
+
+        }
+
+
+        //public override void OnStartLocalPlayer() // this is our player
+        //{
+        //    base.OnStartLocalPlayer();
+
+        //    GameObject cameraObj = GameObject.Instantiate<GameObject>(Resources.Load("CameraPrefab") as ); // add camera
+        //    cameraObj.GetComponent<MyCameraScript>().SetTarget(transform); // setup camera
+
+        //    // add input handler component OR (see Update)
+        //}
+
+        private void SetStickCommandsUsingAutopilot()
+        {
+            // Project the position of the mouse on screen out to some distance.
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 1000f;
+            Vector3 gotoPos = mainCam.ScreenToWorldPoint(mousePos);
+
+            // Use that world position under the mouse as a target point.
+            TurnTowardsPoint(gotoPos);
+
+			// Use the mouse to bank the ship some degrees based on the mouse position.
+			BankShipRelativeToUpVector(mousePos, mainCam.transform.up);
+		}
+
+        /// <summary>
+        /// Ship will roll relative to the given up vector, modified by the mouse position.
+        /// E.g. If the mouse is centered, the ship's up will match the up vector. If the mouse
+        /// is at the right extreme of the screen, it will bank right bankLimit degrees relative
+        /// to the given up vector.
+        /// </summary>
+        /// <param name="mousePos"></param>
+        /// <param name="upVector"></param>
+        private void BankShipRelativeToUpVector(Vector3 mousePos, Vector3 upVector)
+        {
+            // A PID Controller is HIGHLY recommended to get the most out of this.
+            // The "sensitivity" values are poor substitutes for them, but they work well enough for
+            // the sake of a demo. A tuned PID controller on the other hand will give extremely
+            // accurate and responsive ship flight.
+            // See https://en.wikipedia.org/wiki/PID_controller
+            // And https://forum.unity.com/threads/pid-controller.68390/
+            // Video showing the difference it makes: https://www.youtube.com/watch?v=ErPgBHwWffE
+
+            // The ship is meant to bank left/right based on how much the mouse cursor is to the left
+            // or right of the screen. E.g. Mouse at left edge of screen means the ship should try to
+            // bank counterclockwise by bankLimit degrees. 
+
+            // Figure out most position relative to center of screen.
+            // 0 is center, 1 is right, -1 is left.
+            float bankInfluence = (mousePos.x - (Screen.width * 0.5f)) / (Screen.width * 0.5f);
+            bankInfluence = Mathf.Clamp(bankInfluence, -1f, 1f);
+
+            // Throttle modifies the bank angle so that when at idle, the ship just flatly yaws.
+            bankInfluence *= throttle;
+            float bankTarget = bankInfluence * bankLimit;
+
+            // Here's the special sauce. Roll so that the bank target is reached relative to the
+            // up of the camera.
+            float bankError = Vector3.SignedAngle(transform.up, upVector, transform.forward);
+            bankError = bankError - bankTarget;
+
+            // Clamp this to prevent wild inputs.
+            bankError = Mathf.Clamp(bankError * 0.1f, -1f, 1f);
+			bankError = 0f;
+            // Roll to minimze error.
+            roll = bankError * rollSensitivity;
+        }
+
+        /// <summary>
+        /// Pitches and yaws the ship to look at the passed in world position.
+        /// </summary>
+        /// <param name="gotoPos">World position to turn the ship towards.</param>
+        private void TurnTowardsPoint(Vector3 gotoPos)
+        {
+            Vector3 localGotoPos = transform.InverseTransformVector(gotoPos - transform.position).normalized;
+
+            // Note that you would want to use a PID controller for this to make it more responsive.
+            pitch = Mathf.Clamp(-localGotoPos.y * pitchSensitivity, -1f, 1f);
+            yaw = Mathf.Clamp(localGotoPos.x * yawSensitivity, -1f, 1f);
+        }
+
+        /// <summary>
+        /// Uses R and F to raise and lower the throttle.
+        /// </summary>
+        private void UpdateKeyboardThrottle(KeyCode increaseKey, KeyCode decreaseKey)
+        {
+            float target = throttle;
+
+            if (Input.GetKey(increaseKey))
+                target = 1.0f;
+            else if (Input.GetKey(decreaseKey))
+                target = 0.0f;
+
+            throttle = Mathf.MoveTowards(throttle, target, Time.deltaTime * THROTTLE_SPEED);
+        }
+
+        /// <summary>
+        /// Uses the mouse wheel to control the throttle.
+        /// </summary>
+        private void UpdateMouseWheelThrottle()
+        {
+            throttle += Input.GetAxis("Mouse ScrollWheel");
+            throttle = Mathf.Clamp(throttle, 0.0f, 1.0f);
+        }
+
+		private bool allowfire = true;
+
+        [Command]
+		void Cmdshoot()
+		{
+        
+			GameObject new_bullet = Instantiate(Resources.Load<GameObject>("Bullet"), transform.position, transform.rotation);
+			new_bullet.transform.localPosition = new Vector3(0f, 0, 10f);
+            new_bullet.transform.position = transform.position;
+			//new_bullet.gameObject.layer = current_layer;
+			NetworkServer.Spawn(new_bullet);
+            new_bullet.GetComponent<Rigidbody>().velocity = rb.velocity + transform.forward * 100f;
+            Destroy(new_bullet, 3f);
+		}
+
+        public override void OnNetworkDestroy()
+        {
+            Destroy(mainCam.gameObject);
+            base.OnNetworkDestroy();
+        }
+
+
+
+        private IEnumerator Fire()
+		{
+
+			allowfire = false;
+			Cmdshoot();
+			yield return new WaitForSeconds(0.5f);
+			allowfire = true;
+		}
+
+        //private GameObject cameraGO;
+        //public void SetCamera(GameObject _cameraGO)
+        //{
+        //    cameraGO = _cameraGO;
+        //    Camera.SetupCurrent(cameraGO.GetComponent<Camera>());
+        //}
+
+
+
+    }
